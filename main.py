d1ad97aad25996da079b3d4321dc234a3a3aa921
No4vick <MineBor1@yandex.ru>
2022-02-05 17:47:55 +0300

    first version (kinda works)

--- main.py
+++ main.py
@@ -3,40 +3,67 @@ from youtube_dl import YoutubeDL
 
 YDL_OPTIONS = {'noplaylist': 'True'}
 
+formats = {}
+selected_format = {}
+item_list = []
 
-def change_text(sender, app_data):
-    request_param = "https://www.youtube.com/watch?v=PyUcdClB5uc"
+
+def set_format():
+    global formats, selected_format
+    index = item_list.index(dpg.get_value("fetched items"))
+    print(index)
+    selected_format = formats[index]
+    # selected_format = formats
+
+
+def download():
+    print(selected_format)
+    ydl_opts = YDL_OPTIONS
+    ydl_opts['format_id'] = selected_format['format_id']
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download({dpg.get_value("url field")})
+    #TODO REWORK DOWNLOAD!!!
+    #TODO add file selector
+
+
+def fetch_options(sender, app_data):
+    request_param = dpg.get_value("url field")
+    print(f"request param: {request_param}")
     with YoutubeDL(YDL_OPTIONS) as ydl:
         youtube_request = ydl.extract_info(request_param, download=False)
-    print(youtube_request)
-    format_str = ""
-    format_str += f"Title: {youtube_request['title']}\n\n"
-    count = 0
+    # print(youtube_request)
+    global item_list
+    label = f"Title: {youtube_request['title']}"
+    global formats
+    formats = youtube_request['formats']
     for value in youtube_request['formats']:
-        count += 1
         if value['filesize']:
-            format_str += f"{count}. {value['format_note']} ({value['ext']}): {round(int(value['filesize']) / 1024, 2)} mBytes\n"
+            item_list.append(
+                f"{value['format_note']} ({value['ext']}): {round(int(value['filesize']) / 1024, 2)} mBytes")
         else:
-            format_str += f"{value['format_note']} ({value['ext']}): Unknown size\n"
-    dpg.set_value("fetched text", f"{format_str}")
+            item_list.append(f"{value['format_note']} ({value['ext']}): Unknown size")
+    dpg.add_radio_button(parent="main window", items=item_list, tag="fetched items", label=label, callback=set_format)
+    dpg.add_button(parent="main window", label="Download!", tag="download btn", callback=download)
+    #TODO Clear radios maybe?
 
 
 if __name__ == '__main__':
     dpg.create_context()
-    dpg.create_viewport(title='YoutubeDL GUI by No4nick', width=600, height=600, resizable=False)
+    dpg.create_viewport(title='YoutubeDL GUI by No4nick', width=600, height=600)
 
-    with dpg.window(width=600, height=600, no_title_bar=True, no_move=True, no_resize=True):
+    with dpg.window(width=600, height=600, no_title_bar=True, no_move=True, no_resize=True, tag="main window"):
         dpg.add_text("YoutubeDL_XD!!")
-        dpg.add_input_text(label="YouTube URL", hint="YouTube URL")
+        dpg.add_input_text(label="YouTube URL", hint="YouTube URL", tag="url field",
+                           default_value="https://www.youtube.com/watch?v=PyUcdClB5uc")
         dpg.add_button(label="Fetch", tag="fetch btn")
-        dpg.add_text(tag="fetched text")
 
-    with dpg.item_handler_registry(tag="widget handler") as handler:
-        dpg.add_item_clicked_handler(callback=change_text)
+    with dpg.item_handler_registry(tag="fetch btn handler") as handler:
+        dpg.add_item_clicked_handler(callback=fetch_options)
 
     # bind item handler registry to item
-    dpg.bind_item_handler_registry("fetch btn", "widget handler")
+    dpg.bind_item_handler_registry("fetch btn", "fetch btn handler")
 
+    dpg.set_primary_window("main window", True)
     dpg.setup_dearpygui()
     dpg.show_viewport()
     dpg.start_dearpygui()
